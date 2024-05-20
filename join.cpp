0bb749e5a1ce3d61671e007d3c84f1c7ad0dd709
최지민 <127175733+ppoppy@users.noreply.github.com>
2024-05-20 16:58:42 +0900

    Create join.cpp

---
+++ join.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <tread>
+#include <windows.h> //sleep()
+
+using namespace std;
+
+void worker();
+
+void worker() {
+  for (int i = 0; i < 10; ++i){
+    Sleep(1000);
+    cout << i << endl;
+  }
+}
+
+int main {
+  thread t1(worker);  //t1 생성 & 실행
+  t1.join();          //t1 해제, 메인 스레드는 t1이 끝날 때까지 block
+  printf("===end of main()===\n"); //printf()도 사용 가능
+  return 0;
+}
