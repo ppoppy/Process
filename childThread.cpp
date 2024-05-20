a7b62ea218e029e2aec4955b48603976ee551cee
최지민 <127175733+ppoppy@users.noreply.github.com>
2024-05-20 17:06:05 +0900

    Create childThread.cpp

---
+++ childThread.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <windows.h>
+
+using namespace std;
+mutex mtx;
+
+void worker(int, int);
+
+void worker(int id, int delay_ms){
+  for (int i = 0; i < 10; ++i){
+    Sleep(delay_ms);
+    mtx.lock();
+    cout << "thread#" << id << ": " << i << endl;
+    mtx.unlock();
+  }
+}
+
+int main() {
+  thread t1(worker, 1, 1000);
+  thread t2(worker, 2, 300);
+  t1.join();
+  t2.join();
+  printf("===end of main()===\n");
+}
